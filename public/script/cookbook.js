aadc3990df3c6654e7374439543ffb908496c97d
parry-do <34604130+parry-do@users.noreply.github.com>
2018-05-14 10:24:01 -0700

    Trying moving data outside of the function

--- public/script/cookbook.js
+++ public/script/cookbook.js
@@ -4,7 +4,7 @@
 path = "../lessons/"
 
 // Lessons to be added
-lessons = ["Data Types 1", "Data Types 2",]
+var lessons = [["Data Types 1", "//This lesson is empty right now"]];
 
 // The content of each lesson is obtained
 //lessons.forEach(function(lesson, index, array) {
@@ -18,7 +18,7 @@ define([], function() {
     //  addExample(lesson[0], lesson[1]);
     //});
     
-    lessons = [["Data Types 1", "//This lesson is empty right now"]];
+    
     
     var square = "function square() {\n  for (var i=1; i <= 4; i++) {\n    fd(50)\n    lt(90)\n  }\n}"
     addExample("Turtle moves", "fd(50)\nlt(45)\nfd(50)\nrt(45)\nfd(50)")
