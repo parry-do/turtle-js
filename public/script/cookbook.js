f5c75e7024f9c41375f2efa94423d3531fe66d1d
parry-do <34604130+parry-do@users.noreply.github.com>
2018-05-14 14:16:58 -0700

    Compile-time loading of text files by require

--- public/script/cookbook.js
+++ public/script/cookbook.js
@@ -1,29 +1,25 @@
 "use strict";
 
 // Path leading to the lesson files
-path = "../lessons/"
+path = "text!../lessons/";
 
 // Lessons to be added
 //var lessons = [["Data Types 1", "//This lesson is empty right now"]];
-var lessons = ["Data Types 1", "Data Types 2"];
+var files = ["Data Types 1.txt",
+               "Data Types 2.txt"];
 
-// The content of each lesson is obtained
-lessons.forEach(function(lesson, index, array) {
-  array[index] = [lesson, require(path+lesson)]
-});
-
-define([], function() {
+define(files, function() {
+  
   return function Cookbook(editor, repl) {
 
-    //lessons.forEach(function(lesson) {
-    //  addExample(lesson[0], lesson[1]);
-    //});
+    lessons = files.map((e, i) => [e, arguments[i]]});
     
-    
+    lessons.forEach(function(lesson) {
+      addExample(lesson[0], lesson[1]);
+    })
     
     var square = "function square() {\n  for (var i=1; i <= 4; i++) {\n    fd(50)\n    lt(90)\n  }\n}"
     addExample("Turtle moves", "fd(50)\nlt(45)\nfd(50)\nrt(45)\nfd(50)")
-    addExample(lessons[0][0], lessons[0][1])
     addExample("Square", square + "\nsquare()")
     addExample("Spiral", "clear()\ni = 1\nwhile (i < 300) {\n  fd(i*1)\n  lt(i*1)\n  i++\n}")
     addExample("Clear", "clear()")
